db15c459df182376bbd4e37551532feaa423e43e
Radu Dumitru Boboia <rd.boboia@alumnos.upm.es>
2019-03-29 22:30:31 +0100

    MandelbrotsetGenerator resubido.

--- src/main/java/tfg/fractalgenerator/mandelbrotset/MandelbrotsetGenerator.java
+++ src/main/java/tfg/fractalgenerator/mandelbrotset/MandelbrotsetGenerator.java
@@ -1,5 +1,72 @@
 package tfg.fractalgenerator.mandelbrotset;
 
-public class MandelbrotsetGenerator {
+import java.awt.Color;
+import java.awt.image.BufferedImage;
 
+/**
+ * A simple module that generates the Mandelbrot Set and stores it in a given
+ * {@link BufferedImage}.
+ * <br>
+ * It takes the image and the depth as parameters. The height and width is
+ * based on the image's size.
+ * 
+ * @author -$BOSS$-
+ */
+public class MandelbrotsetGenerator {
+	/**
+	 * The image where the data will be stored.
+	 */
+	private BufferedImage img;
+	/**
+	 * The depth (number of iterations for each pixel).
+	 */
+	private int depth;
+	/**
+	 * The height (based on the image's height)
+	 */
+	private int height;
+	/**
+	 * The width (based on the image's width)
+	 */
+	private int width;
+	
+	/**
+	 * Main constructor for the Mandelbrot set generator.
+	 * @param img the image to write the data on.
+	 * @param depth the number of iterations per pixel.
+	 */
+	public MandelbrotsetGenerator(BufferedImage img, int depth) {
+		this.img = img;
+		this.depth = depth;
+		this.height = img.getHeight();
+		this.width = img.getWidth();
+	}
+	
+	/**
+	 * Generates each pixel's value of the Mandelbrot set and stores it into
+	 * the given image.
+	 * It uses the HSBtoRGB method from {@link java.awt.Color} to generate a
+	 * nice looking color scheme while performing relatively few iterations
+	 * per pixel.
+	 */
+	public void generate() {
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				double x0 = (j - width / 2d) * 4d / width;
+				double y0 = (j - height / 2d) * 4d / width;
+				double x = 0;
+				double y = 0;
+				int iteration = 0;
+				
+				while (x * x + y * y <= 4 && iteration < depth) {
+					double xtemp = x * x - y * y + x0;
+					y = 2 * x * y + y0;
+					x = xtemp;
+					iteration++;
+				}
+
+				img.setRGB(j, i, Color.HSBtoRGB(iteration/(float)depth, 1, iteration/(iteration+8f)));
+			}
+		}
+	}
 }
