99828186e2a4cba645c83390e6d9ad7154ff900c
Radu Dumitru Boboia <rd.boboia@alumnos.upm.es>
2019-03-31 22:44:03 +0200

    Documentación (Javadoc) del panel ModeSelectionPanel.

--- src/main/java/tfg/fractalgenerator/gui/panels/ModeSelectionPanel.java
+++ src/main/java/tfg/fractalgenerator/gui/panels/ModeSelectionPanel.java
@@ -8,11 +8,30 @@ import tfg.fractalgenerator.gui.MandelbrotSetGUI;
 
 import javax.swing.JButton;
 
+/**
+ * This view is meant to be a simple selector for different modes of operation
+ * of the application. It's a simple {@link JPanel} with two buttons, so only
+ * two options to choose from. Each one leads to another view.
+ * 
+ * @author -$BOSS$-
+ */
 public class ModeSelectionPanel extends JPanel {
+	/**
+	 * Serial version number. Using the default one.
+	 */
 	private static final long serialVersionUID = 1L;
 	
+	/**
+	 * Static reference to the name of this class which is used as the name
+	 * of the component. The name is based on the {@code Class}'s name. Useful
+	 * for the card layout since it makes a lot easier to have access to a
+	 * component's name. 
+	 */
 	public static final String NAME = ModeSelectionPanel.class.getSimpleName();
 
+	/**
+	 * Initialization of the Panel and it's layout.
+	 */
 	public ModeSelectionPanel() {
 		this.setSize(MandelbrotSetGUI.size);
 		this.setName(NAME);
