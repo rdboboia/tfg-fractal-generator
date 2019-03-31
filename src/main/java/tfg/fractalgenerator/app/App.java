9925df13c17094cd550d14ab014fbad872b40476
Radu Dumitru Boboia <rd.boboia@alumnos.upm.es>
2019-03-31 21:55:04 +0200

    Documentación (Javadoc) de la clase App (main).

--- src/main/java/tfg/fractalgenerator/app/App.java
+++ src/main/java/tfg/fractalgenerator/app/App.java
@@ -2,7 +2,17 @@ package tfg.fractalgenerator.app;
 
 import tfg.fractalgenerator.gui.MandelbrotSetGUI;
 
+/**
+ * Main class for the Mandelbrot Set generation application.
+ * 
+ * @author -$BOSS$-
+ */
 public class App {
+	/**
+	 * Main method for starting the application.
+	 * It launches the GUI which for now is the only way to use the application.
+	 * @param args the arguments are ignored (for now).
+	 */
 	public static void main(String[] args) {
 		MandelbrotSetGUI gui = MandelbrotSetGUI.getInstance();
 		gui.setVisible(true);
