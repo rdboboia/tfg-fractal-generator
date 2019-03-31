880b3e0dbf603eedbf30adacd516a29c35e92ada
Radu Dumitru Boboia <rd.boboia@alumnos.upm.es>
2019-03-31 16:38:19 +0200

    Implementada interfaz de selección de modo (fichero o tiempo real).

---
+++ src/main/java/tfg/fractalgenerator/gui/panels/ModeSelectionPanel.java
@@ -0,0 +1,48 @@
+package tfg.fractalgenerator.gui.panels;
+
+import javax.swing.JPanel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+
+import tfg.fractalgenerator.gui.MandelbrotSetGUI;
+
+import javax.swing.JButton;
+
+public class ModeSelectionPanel extends JPanel {
+	private static final long serialVersionUID = 1L;
+	
+	public static final String NAME = ModeSelectionPanel.class.getSimpleName();
+
+	public ModeSelectionPanel() {
+		this.setSize(MandelbrotSetGUI.size);
+		
+		JButton btnNewButton = new JButton("Visualización en tiempo real");
+		btnNewButton.addActionListener(e -> MandelbrotSetGUI.getInstance().changeCard(RealtimeViewPanel.NAME));
+		
+		JButton btnSoloExportarA = new JButton("Solo exportar a imágen");
+		btnSoloExportarA.addActionListener(e -> MandelbrotSetGUI.getInstance().changeCard(FileOnlyPanel.NAME));
+		
+		GroupLayout groupLayout = new GroupLayout(this);
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(299)
+					.addComponent(btnNewButton, GroupLayout.DEFAULT_SIZE, 300, Short.MAX_VALUE)
+					.addGap(82)
+					.addComponent(btnSoloExportarA, GroupLayout.DEFAULT_SIZE, 300, Short.MAX_VALUE)
+					.addGap(299))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(260)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnSoloExportarA, GroupLayout.DEFAULT_SIZE, 200, Short.MAX_VALUE)
+						.addComponent(btnNewButton, GroupLayout.DEFAULT_SIZE, 200, Short.MAX_VALUE))
+					.addGap(260))
+		);
+		setLayout(groupLayout);
+
+	}
+
+}
