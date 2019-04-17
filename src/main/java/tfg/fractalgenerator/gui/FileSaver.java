ca7a427756ceea9cb44e17b23c5bb23cef36bb89
Radu Dumitru Boboia <rd.boboia@alumnos.upm.es>
2019-04-17 13:10:29 +0200

    Implementación de comprobación de extensión y check de archivo destino.

--- src/main/java/tfg/fractalgenerator/gui/FileSaver.java
+++ src/main/java/tfg/fractalgenerator/gui/FileSaver.java
@@ -10,6 +10,7 @@ import javax.swing.JOptionPane;
 
 import tfg.fractalgenerator.exportimage.ImageExport;
 import tfg.fractalgenerator.exportimage.ImageFormat;
+import tfg.fractalgenerator.exportimage.ImageFormatParser;
 
 /**
  * Provides a method to handle file saving with a GUI using the {@link FileDialog},
@@ -27,8 +28,12 @@ public class FileSaver {
 	/**
 	 * Displays a confirmation message when the file is successfully saved.
 	 */
-	private static void showSuccessMessage() {
-		JOptionPane.showMessageDialog(null, "El archivo se ha guardado correctamente.", "Guardado completado", JOptionPane.INFORMATION_MESSAGE);
+	private static void checkFile(String fileDirectory, String fileName, String fileExtension) {
+		fileName = removeFileExtensionIfAlreadyExists(fileName, fileExtension);
+		if (Paths.get(fileDirectory + "\\" + fileName + "." + fileExtension).toFile().exists())
+			JOptionPane.showMessageDialog(null, "El archivo se ha guardado correctamente.", "Guardado completado", JOptionPane.INFORMATION_MESSAGE);
+		else
+			JOptionPane.showMessageDialog(null, "El archivo no se ha podido guardar.", "Error", JOptionPane.ERROR_MESSAGE);
 	}
 	
 	/**
@@ -42,19 +47,36 @@ public class FileSaver {
 
 		String fileName = d.getFile();
 		if (fileName != null) {
+			String directory = d.getDirectory();
+			String format = ImageFormatParser.getImageFormat(imageFormat);
+			
 			try {
-				if (Paths.get(d.getDirectory() + "\\" + d.getFile()).toFile().exists()) {
+				if (Paths.get(directory + "\\" + fileName).toFile().exists()) {
 					if (JOptionPane.showConfirmDialog(null, "¿Desea sobreescribir el archivo?", "El archivo ya existe", JOptionPane.OK_CANCEL_OPTION) == JOptionPane.OK_OPTION) {
-						ImageExport.export(image, imageFormat, d.getDirectory(), fileName, true);
-						showSuccessMessage();
+						ImageExport.export(image, imageFormat, directory, fileName, true);
+						checkFile(d.getDirectory(), d.getFile(), format);
 					}
 				} else {
 					ImageExport.export(image, imageFormat, d.getDirectory(), fileName, false);
-					showSuccessMessage();
+					checkFile(d.getDirectory(), d.getFile(), format);
 				}
 			} catch (IOException e) {
 				JOptionPane.showMessageDialog(null, "No se pudo guardar el archivo.", "Error al guardar", JOptionPane.ERROR_MESSAGE);
 			}
 		}
+	}
+	
+	/**
+	 * Removes the extension from the file name.
+	 * @param fileName the file name to be checked.
+	 * @param extension the extension to be removed.
+	 * @return the file name without the extension.
+	 */
+	public static String removeFileExtensionIfAlreadyExists(String fileName, String extension) {
+		if (fileName.toLowerCase().endsWith(extension)) {
+			fileName = fileName.substring(0, fileName.length() - (extension.length() + 1));
+		}
+		
+		return fileName;
 	}
 }
