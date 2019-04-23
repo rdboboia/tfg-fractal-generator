dd98d72d17bdf59f1b21696bc583d04e8352aff5
Radu Dumitru Boboia <rd.boboia@alumnos.upm.es>
2019-04-23 23:11:02 +0200

    Deshabilitado el test que lanza una ventana.

--- src/test/java/tfg/fractalgenerator/gui/MandelbrotSetGUITest.java
+++ src/test/java/tfg/fractalgenerator/gui/MandelbrotSetGUITest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.security.InvalidParameterException;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import tfg.fractalgenerator.gui.panels.ModeSelectionPanel;
@@ -14,10 +15,11 @@ class MandelbrotSetGUITest {
 		assertTrue(MandelbrotSetGUI.getInstance().changeCard(ModeSelectionPanel.NAME),
 				"The panel should change correctly since it exists in the panel list");
 	}
-
+	
+	@Disabled // Since the method launches a JOptionPane there's no way to automate this test; human interaction is needed.
 	@Test
 	void incorrectChangeCardMethodTest() {
-		assertThrows(InvalidParameterException.class, () -> MandelbrotSetGUI.getInstance().changeCard("mal"),
-				"The method should give an InvalidParameterException since the given panel name doesn't exist in the panels list.");
+		assertThrows(InvalidParameterException.class, () -> MandelbrotSetGUI.getInstance().changeCard("very legit panel name"),
+				"The method should throw an InvalidParameterException since the given panel name doesn't exist in the panels list.");
 	}
 }
