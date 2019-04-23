85308bf2aba69edd38aab1bc6179029a2108ee79
Radu Dumitru Boboia <rd.boboia@alumnos.upm.es>
2019-04-23 23:13:32 +0200

    Agregada imagen contra la que se prueba si el resultado es el esperado.

---
+++ src/test/java/tfg/fractalgenerator/mandelbrotset/MandelbrotsetGeneratorThreadManagerTest.java
@@ -0,0 +1,59 @@
+package tfg.fractalgenerator.mandelbrotset;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import tfg.fractalgenerator.exportimage.ImageExport;
+import tfg.fractalgenerator.exportimage.ImageFormat;
+
+class MandelbrotsetGeneratorThreadManagerTest {
+
+	@Test
+	void test() {
+		try {
+			BufferedImage imageTest = ImageIO.read(new File("imageTest.png"));
+			byte[] imageTestPixels = ((DataBufferByte) imageTest.getRaster().getDataBuffer()).getData();
+			
+			BufferedImage renderedImage = new BufferedImage(3840, 2160, BufferedImage.TYPE_3BYTE_BGR);
+			MandelbrotsetGeneratorThreadManager.generate(renderedImage, 360, 360, new MandelbrotsetPosition(0, 0, 3, 4d / 1280));
+			byte[] renderedImagePixels = ((DataBufferByte) renderedImage.getRaster().getDataBuffer()).getData();
+			
+			ImageExport.export(imageTest, ImageFormat.PNG, "Z:\\", "fractalOriginal", true);
+			ImageExport.export(renderedImage, ImageFormat.PNG, "Z:\\", "fractalNuevo", true);
+			
+			assertArrayEquals(imageTestPixels, renderedImagePixels);
+		} catch (IOException e) {
+			fail("Exception thrown!");
+		}
+	}
+	
+	@Disabled
+	@Test
+	void performanceTest() {
+		BufferedImage image = new BufferedImage(19200, 10800, BufferedImage.TYPE_INT_RGB);
+		MandelbrotsetPosition position = new MandelbrotsetPosition();
+		position.setZoom(10);
+		
+		long s = System.currentTimeMillis();
+		
+		MandelbrotsetGeneratorThreadManager.generate(image, 3600, 360, position);
+		
+		long f = System.currentTimeMillis();
+		System.out.println((f-s) + " ms.");
+		
+		try {
+			ImageExport.export(image, ImageFormat.PNG, "Z:\\", "fractal", true);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
