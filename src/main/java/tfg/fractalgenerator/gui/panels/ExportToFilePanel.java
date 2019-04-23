2ba13f4358dcef953a5790579fe8cebffb2bf758
Radu Dumitru Boboia <rd.boboia@alumnos.upm.es>
2019-04-23 23:10:26 +0200

    Implementado y documentado el nuevo panel de exportar a fichero.

--- src/main/java/tfg/fractalgenerator/gui/panels/ExportToFilePanel.java
+++ src/main/java/tfg/fractalgenerator/gui/panels/ExportToFilePanel.java
@@ -5,7 +5,9 @@ import javax.swing.JPanel;
 import tfg.fractalgenerator.exportimage.ImageFormat;
 import tfg.fractalgenerator.gui.FileSaver;
 import tfg.fractalgenerator.gui.MandelbrotSetGUI;
-import tfg.fractalgenerator.mandelbrotset.MandelbrotsetGenerator;
+import tfg.fractalgenerator.gui.panels.store.GenerationParametersStore;
+import tfg.fractalgenerator.mandelbrotset.MandelbrotsetGeneratorThreadManager;
+import tfg.fractalgenerator.mandelbrotset.MandelbrotsetPosition;
 
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
@@ -21,12 +23,29 @@ import javax.swing.DefaultComboBoxModel;
 import java.awt.Font;
 import javax.swing.border.TitledBorder;
 
-public class FileOnlyPanel extends JPanel {
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+
+public class ExportToFilePanel extends JPanel {
+	/**
+	 * Serial version number. Using the default one.
+	 */
 	private static final long serialVersionUID = 1L;
 	
-	public static final String NAME = FileOnlyPanel.class.getSimpleName();
-
-	public FileOnlyPanel() {
+	/**
+	 * Static reference to the name of this class which is used as the name
+	 * of the component. The name is based on the {@code Class}'s name. Useful
+	 * for the card layout since it makes a lot easier to have access to a
+	 * component's name. 
+	 */
+	public static final String NAME = ExportToFilePanel.class.getSimpleName();
+	
+	/**
+	 * Initialization of the Panel and it's layout.
+	 * It also contains the {@link ActionListener} for each button.
+	 */
+	public ExportToFilePanel() {
 		this.setSize(MandelbrotSetGUI.size);
 		this.setName(NAME);
 		
@@ -42,13 +61,13 @@ public class FileOnlyPanel extends JPanel {
 		JLabel lblResolution = new JLabel("Resolución:");
 		
 		JSpinner spinnerWidth = new JSpinner();
-		spinnerWidth.setModel(new SpinnerNumberModel(new Integer(3840), new Integer(1), null, new Integer(1)));
+		spinnerWidth.setModel(new SpinnerNumberModel(3840, 1, null, 1));
 		
 		JLabel lblWidth = new JLabel("(ancho)");
 		lblWidth.setHorizontalAlignment(SwingConstants.CENTER);
 		
 		JSpinner spinnerHeight = new JSpinner();
-		spinnerHeight.setModel(new SpinnerNumberModel(new Integer(2160), new Integer(1), null, new Integer(1)));
+		spinnerHeight.setModel(new SpinnerNumberModel(2160, 1, null, 1));
 		
 		JLabel lblHeight = new JLabel("(alto)");
 		lblHeight.setHorizontalAlignment(SwingConstants.CENTER);
@@ -56,7 +75,7 @@ public class FileOnlyPanel extends JPanel {
 		JLabel lblDepth = new JLabel("Profundidad:");
 		
 		JSpinner spinnerDepth = new JSpinner();
-		spinnerDepth.setModel(new SpinnerNumberModel(new Integer(360), new Integer(1), null, new Integer(1)));
+		spinnerDepth.setModel(new SpinnerNumberModel(360, 1, null, 1));
 		
 		JLabel lblFormat = new JLabel("Formato:");
 		
@@ -64,27 +83,65 @@ public class FileOnlyPanel extends JPanel {
 		comboBoxFormat.setModel(new DefaultComboBoxModel<ImageFormat>(ImageFormat.values()));
 		comboBoxFormat.setSelectedIndex(comboBoxFormat.getModel().getSize()-1);
 		
+		JPanel propertiesPanel = new JPanel();
+		propertiesPanel.setBorder(new TitledBorder(null, "Propiedades", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		
+		JPanel positionPanel = new JPanel();
+		positionPanel.setBorder(new TitledBorder(null, "Posici\u00F3n", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		
+		JLabel lblEjeX = new JLabel("Eje x:");
+		
+		JSpinner spinnerXAxis = new JSpinner();
+		spinnerXAxis.setModel(new SpinnerNumberModel(0, null, null, 1));
+		
+		JLabel lblEjeY = new JLabel("Eje y:");
+		
+		JSpinner spinnerYAxis = new JSpinner();
+		spinnerYAxis.setModel(new SpinnerNumberModel(0, null, null, 1));
+		
+		JLabel lblZoom = new JLabel("Zoom:");
+		
+		JSpinner spinnerZoom = new JSpinner();
+		spinnerZoom.setModel(new SpinnerNumberModel(1, 1, null, 1));
+		
+		JLabel lblEscala = new JLabel("Escala:");
+		
+		JSpinner spinnerScale = new JSpinner();
+		spinnerScale.setModel(new SpinnerNumberModel(0.003125, null, null, 1));
+		
+		JLabel lblColores = new JLabel("Colores:");
+		
+		JSpinner spinnerColorDepth = new JSpinner();
+		spinnerColorDepth.setModel(new SpinnerNumberModel(360, 1, null, 1));
+		
 		JButton btnGenerateAndExport = new JButton("Generar y exportar");
+		btnGenerateAndExport.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		btnGenerateAndExport.addActionListener(e -> {
 			new Thread() {
 				@Override
 				public void run() {
+					btnGenerateAndExport.setEnabled(false);
+					
 					lblActualStatus.setText("creando imagen...");
 					BufferedImage image = new BufferedImage((int)spinnerWidth.getValue(), (int)spinnerHeight.getValue(), BufferedImage.TYPE_INT_RGB);
 					
 					lblActualStatus.setText("generando fractal...");
-					MandelbrotsetGenerator.generate(image, (int)spinnerDepth.getValue());
+					MandelbrotsetPosition position = new MandelbrotsetPosition();
+					position.setPosx((double)spinnerXAxis.getValue());
+					position.setPosy((double)spinnerYAxis.getValue());
+					position.setZoom((double)spinnerZoom.getValue());
+					position.setScale((double)spinnerScale.getValue());
+					MandelbrotsetGeneratorThreadManager.generate(image, (int)spinnerDepth.getValue(), (int)spinnerColorDepth.getValue(), position);
 					
 					lblActualStatus.setText("exportación de archivo...");
 					FileSaver.saveFile(image, (ImageFormat)comboBoxFormat.getSelectedItem());
 					
 					lblActualStatus.setText("finalizado.");
+					
+					btnGenerateAndExport.setEnabled(true);
 				}
 			}.start();
 		});
-		
-		JPanel propertiesPanel = new JPanel();
-		propertiesPanel.setBorder(new TitledBorder(null, "Propiedades", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		
 		GroupLayout groupLayout = new GroupLayout(this);
 		groupLayout.setHorizontalGroup(
@@ -93,7 +150,7 @@ public class FileOnlyPanel extends JPanel {
 					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
 						.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
 							.addContainerGap()
-							.addComponent(propertiesPanel, GroupLayout.DEFAULT_SIZE, 1260, Short.MAX_VALUE))
+							.addComponent(btnGenerateAndExport, GroupLayout.DEFAULT_SIZE, 1260, Short.MAX_VALUE))
 						.addComponent(btnReturn, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)
 						.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
 							.addContainerGap()
@@ -102,7 +159,9 @@ public class FileOnlyPanel extends JPanel {
 							.addComponent(lblActualStatus, GroupLayout.DEFAULT_SIZE, 1195, Short.MAX_VALUE))
 						.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
 							.addContainerGap()
-							.addComponent(btnGenerateAndExport, GroupLayout.DEFAULT_SIZE, 1260, Short.MAX_VALUE)))
+							.addComponent(propertiesPanel, GroupLayout.PREFERRED_SIZE, 627, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(positionPanel, GroupLayout.DEFAULT_SIZE, 627, Short.MAX_VALUE)))
 					.addContainerGap())
 		);
 		groupLayout.setVerticalGroup(
@@ -110,9 +169,11 @@ public class FileOnlyPanel extends JPanel {
 				.addGroup(groupLayout.createSequentialGroup()
 					.addComponent(btnReturn)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(propertiesPanel, GroupLayout.PREFERRED_SIZE, 219, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED, 278, Short.MAX_VALUE)
-					.addComponent(btnGenerateAndExport, GroupLayout.PREFERRED_SIZE, 155, GroupLayout.PREFERRED_SIZE)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(propertiesPanel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(positionPanel, GroupLayout.PREFERRED_SIZE, 390, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addComponent(btnGenerateAndExport, GroupLayout.DEFAULT_SIZE, 244, Short.MAX_VALUE)
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblStatus)
@@ -120,66 +181,118 @@ public class FileOnlyPanel extends JPanel {
 					.addContainerGap())
 		);
 		
+		GroupLayout gl_positionPanel = new GroupLayout(positionPanel);
+		gl_positionPanel.setHorizontalGroup(
+			gl_positionPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_positionPanel.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_positionPanel.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblZoom)
+						.addComponent(lblEscala)
+						.addComponent(lblEjeY)
+						.addComponent(lblEjeX))
+					.addGap(31)
+					.addGroup(gl_positionPanel.createParallelGroup(Alignment.LEADING)
+						.addComponent(spinnerXAxis, GroupLayout.DEFAULT_SIZE, 530, Short.MAX_VALUE)
+						.addComponent(spinnerYAxis, GroupLayout.DEFAULT_SIZE, 530, Short.MAX_VALUE)
+						.addComponent(spinnerZoom, GroupLayout.DEFAULT_SIZE, 530, Short.MAX_VALUE)
+						.addComponent(spinnerScale, GroupLayout.DEFAULT_SIZE, 530, Short.MAX_VALUE))
+					.addContainerGap())
+		);
+		gl_positionPanel.setVerticalGroup(
+			gl_positionPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_positionPanel.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_positionPanel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblEjeX)
+						.addComponent(spinnerXAxis, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addGroup(gl_positionPanel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblEjeY)
+						.addComponent(spinnerYAxis, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addGroup(gl_positionPanel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblZoom)
+						.addComponent(spinnerZoom, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addGroup(gl_positionPanel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblEscala)
+						.addComponent(spinnerScale, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(222, Short.MAX_VALUE))
+		);
+		positionPanel.setLayout(gl_positionPanel);
 		
 		GroupLayout gl_propertiesPanel = new GroupLayout(propertiesPanel);
 		gl_propertiesPanel.setHorizontalGroup(
 			gl_propertiesPanel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_propertiesPanel.createSequentialGroup()
-					.addContainerGap(498, Short.MAX_VALUE)
+					.addContainerGap()
 					.addGroup(gl_propertiesPanel.createParallelGroup(Alignment.LEADING)
-						.addGroup(Alignment.TRAILING, gl_propertiesPanel.createSequentialGroup()
+						.addGroup(gl_propertiesPanel.createSequentialGroup()
 							.addComponent(lblResolution)
-							.addGap(10)
+							.addGap(50)
 							.addComponent(spinnerWidth, GroupLayout.PREFERRED_SIZE, 62, GroupLayout.PREFERRED_SIZE)
-							.addGap(4)
+							.addPreferredGap(ComponentPlacement.RELATED)
 							.addComponent(lblWidth)
-							.addGap(4)
+							.addPreferredGap(ComponentPlacement.RELATED)
 							.addComponent(spinnerHeight, GroupLayout.PREFERRED_SIZE, 62, GroupLayout.PREFERRED_SIZE)
-							.addGap(4)
-							.addComponent(lblHeight)
-							.addGap(486))
-						.addGroup(Alignment.TRAILING, gl_propertiesPanel.createSequentialGroup()
-							.addComponent(lblFormat)
-							.addGap(4)
-							.addComponent(comboBoxFormat, GroupLayout.PREFERRED_SIZE, 102, GroupLayout.PREFERRED_SIZE)
-							.addGap(543))
-						.addGroup(Alignment.TRAILING, gl_propertiesPanel.createSequentialGroup()
-							.addComponent(lblDepth)
-							.addGap(4)
-							.addComponent(spinnerDepth, GroupLayout.PREFERRED_SIZE, 62, GroupLayout.PREFERRED_SIZE)
-							.addGap(554))))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(lblHeight))
+						.addGroup(gl_propertiesPanel.createSequentialGroup()
+							.addGroup(gl_propertiesPanel.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblDepth)
+								.addComponent(lblColores))
+							.addGap(43)
+							.addGroup(gl_propertiesPanel.createParallelGroup(Alignment.LEADING, false)
+								.addComponent(spinnerDepth, GroupLayout.DEFAULT_SIZE, 199, Short.MAX_VALUE)
+								.addComponent(comboBoxFormat, 0, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(spinnerColorDepth)))
+						.addComponent(lblFormat))
+					.addContainerGap(934, Short.MAX_VALUE))
 		);
 		gl_propertiesPanel.setVerticalGroup(
 			gl_propertiesPanel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_propertiesPanel.createSequentialGroup()
 					.addContainerGap()
-					.addGroup(gl_propertiesPanel.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_propertiesPanel.createSequentialGroup()
-							.addGap(3)
-							.addComponent(lblResolution))
+					.addGroup(gl_propertiesPanel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblResolution)
 						.addComponent(spinnerWidth, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addGroup(gl_propertiesPanel.createSequentialGroup()
-							.addGap(3)
-							.addComponent(lblWidth))
+						.addComponent(lblWidth)
 						.addComponent(spinnerHeight, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addGroup(gl_propertiesPanel.createSequentialGroup()
-							.addGap(3)
-							.addComponent(lblHeight)))
+						.addComponent(lblHeight))
 					.addGap(18)
-					.addGroup(gl_propertiesPanel.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_propertiesPanel.createSequentialGroup()
-							.addGap(3)
-							.addComponent(lblDepth))
+					.addGroup(gl_propertiesPanel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblDepth)
 						.addComponent(spinnerDepth, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addGap(18)
-					.addGroup(gl_propertiesPanel.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_propertiesPanel.createSequentialGroup()
-							.addGap(3)
-							.addComponent(lblFormat))
+					.addGroup(gl_propertiesPanel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblColores)
+						.addComponent(spinnerColorDepth, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addGroup(gl_propertiesPanel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblFormat)
 						.addComponent(comboBoxFormat, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap(361, Short.MAX_VALUE))
+					.addContainerGap(222, Short.MAX_VALUE))
 		);
 		propertiesPanel.setLayout(gl_propertiesPanel);
 		setLayout(groupLayout);
+		
+		addComponentListener(new ComponentAdapter() {
+			@Override
+			public void componentShown(ComponentEvent e) {
+				GenerationParametersStore store = GenerationParametersStore.getInstance();
+				
+				spinnerWidth.setValue(store.getWidth());
+				spinnerHeight.setValue(store.getHeight());
+				
+				spinnerXAxis.setValue(store.getPosition().getPosx());
+				spinnerYAxis.setValue(store.getPosition().getPosy());
+				spinnerZoom.setValue(store.getPosition().getZoom());
+				spinnerScale.setValue(store.getPosition().getScale());
+				
+				spinnerDepth.setValue(store.getDepth());
+				spinnerColorDepth.setValue(store.getColorDepth());
+			}
+		});
 	}
 }
