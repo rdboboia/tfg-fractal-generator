61870710cbe94470d4e09128d75f567959b58d70
Radu Dumitru Boboia <rd.boboia@alumnos.upm.es>
2019-04-24 18:08:14 +0200

    Agregado nuevo test y algún comentario explicativo.

--- src/test/java/tfg/fractalgenerator/mandelbrotset/MandelbrotsetGeneratorThreadManagerTest.java
+++ src/test/java/tfg/fractalgenerator/mandelbrotset/MandelbrotsetGeneratorThreadManagerTest.java
@@ -18,7 +18,7 @@ import tfg.fractalgenerator.exportimage.ImageFormat;
 class MandelbrotsetGeneratorThreadManagerTest {
 
 	@Test
-	void test() {
+	void defaultNumberOfThreadsTest() {
 		try {
 			BufferedImage imageTest = ImageIO.read(new File("imageTest.png"));
 			byte[] imageTestPixels = ((DataBufferByte) imageTest.getRaster().getDataBuffer()).getData();
@@ -27,6 +27,47 @@ class MandelbrotsetGeneratorThreadManagerTest {
 			MandelbrotsetGeneratorThreadManager.generate(renderedImage, 360, 360, new MandelbrotsetPosition(0, 0, 3, 4d / 1280));
 			byte[] renderedImagePixels = ((DataBufferByte) renderedImage.getRaster().getDataBuffer()).getData();
 			
+			// Useful for manual inspection when the test fails.
+//			ImageExport.export(imageTest, ImageFormat.PNG, "Z:\\", "fractalOriginal", true);
+//			ImageExport.export(renderedImage, ImageFormat.PNG, "Z:\\", "fractalNuevo", true);
+			
+			assertArrayEquals(imageTestPixels, renderedImagePixels);
+		} catch (IOException e) {
+			fail("Exception thrown!");
+		}
+	}
+	
+	@Test
+	void oneThreadOnlyTest() {
+		try {
+			BufferedImage imageTest = ImageIO.read(new File("imageTest.png"));
+			byte[] imageTestPixels = ((DataBufferByte) imageTest.getRaster().getDataBuffer()).getData();
+			
+			BufferedImage renderedImage = new BufferedImage(3840, 2160, BufferedImage.TYPE_3BYTE_BGR);
+			MandelbrotsetGeneratorThreadManager.generate(renderedImage, 360, 360, new MandelbrotsetPosition(0, 0, 3, 4d / 1280), 1);
+			byte[] renderedImagePixels = ((DataBufferByte) renderedImage.getRaster().getDataBuffer()).getData();
+			
+			// Useful for manual inspection when the test fails.
+//			ImageExport.export(imageTest, ImageFormat.PNG, "Z:\\", "fractalOriginal", true);
+//			ImageExport.export(renderedImage, ImageFormat.PNG, "Z:\\", "fractalNuevo", true);
+			
+			assertArrayEquals(imageTestPixels, renderedImagePixels);
+		} catch (IOException e) {
+			fail("Exception thrown!");
+		}
+	}
+	
+	@Test
+	void customNumberOfThreadsTest() {
+		try {
+			BufferedImage imageTest = ImageIO.read(new File("imageTest.png"));
+			byte[] imageTestPixels = ((DataBufferByte) imageTest.getRaster().getDataBuffer()).getData();
+			
+			BufferedImage renderedImage = new BufferedImage(3840, 2160, BufferedImage.TYPE_3BYTE_BGR);
+			MandelbrotsetGeneratorThreadManager.generate(renderedImage, 360, 360, new MandelbrotsetPosition(0, 0, 3, 4d / 1280), 3);
+			byte[] renderedImagePixels = ((DataBufferByte) renderedImage.getRaster().getDataBuffer()).getData();
+			
+			// Useful for manual inspection when the test fails.
 //			ImageExport.export(imageTest, ImageFormat.PNG, "Z:\\", "fractalOriginal", true);
 //			ImageExport.export(renderedImage, ImageFormat.PNG, "Z:\\", "fractalNuevo", true);
 			
@@ -38,7 +79,7 @@ class MandelbrotsetGeneratorThreadManagerTest {
 	
 	@Disabled
 	@Test
-	void performanceTest() {
+	void performanceTest() { // Used only to check generation time while trying to optimize the generation algorithm.
 		BufferedImage image = new BufferedImage(19200, 10800, BufferedImage.TYPE_INT_RGB);
 		MandelbrotsetPosition position = new MandelbrotsetPosition();
 		position.setZoom(10);
