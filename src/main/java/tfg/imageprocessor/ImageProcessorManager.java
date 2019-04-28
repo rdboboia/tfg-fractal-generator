28e082e22abce0e2c6b96e083d99bd14d3371979
Radu Dumitru Boboia <rd.boboia@alumnos.upm.es>
2019-04-28 18:13:40 +0200

    Traidos archivos necesarios de la rama imageProcessing

---
+++ src/main/java/tfg/imageprocessor/ImageProcessorManager.java
@@ -0,0 +1,50 @@
+package tfg.imageprocessor;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+
+/**
+ * A class to manage the {@link ImageProcessor} module. It takes an
+ * {@link BufferedImage} and extracts the needed data that the
+ * {@link ImageProcessor} will use to process the image. It also receives the
+ * {@link ProcessingMode} in order to decide which processing method should be
+ * called.
+ * 
+ * @author -$BOSS$-
+ */
+public class ImageProcessorManager {
+	/**
+	 * Private constructor. No instances allowed.
+	 */
+	private ImageProcessorManager() {
+	}
+	
+	/**
+	 * It extracts the RGB(A) byte array data from the image and, based on the
+	 * {@link ProcessingMode} mode, calls the needed method of the
+	 * {@link ImageProcessor} class.
+	 * 
+	 * @param image the image to be processed.
+	 * @param mode the desired processing mode of type {@link ProcessingMode}.
+	 */
+	public static void process(BufferedImage image, ProcessingMode mode) {
+		byte[] pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+		
+		int step;
+		if (image.getAlphaRaster() != null)
+			step = 4;
+		else
+			step = 3;
+		
+		switch (mode) {
+			case COLOR_INVERSION:
+				ImageProcessor.invertColors(pixels, step);
+				break;
+			case GRAYSCALE:
+				ImageProcessor.convertToGrayscale(pixels, step);
+				break;
+			default:
+				break;
+		}
+	}
+}
---
+++ src/test/java/tfg/imageprocessor/ImageProcessorManagerTest.java
@@ -0,0 +1,61 @@
+package tfg.imageprocessor;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+
+import org.junit.jupiter.api.Test;
+
+class ImageProcessorManagerTest {
+	private static final String ASSERTION_ERROR_MESSAGE = "Wrong value. Check if the executed mode is the expected one.";
+
+	@Test
+	void invertColorsTest() {
+		BufferedImage image = new BufferedImage(1000, 1200, BufferedImage.TYPE_3BYTE_BGR);
+		byte[] bytes = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+		byte[] bytesCopy = bytes.clone();
+		
+		ImageProcessor.invertColors(bytesCopy, 3);
+		ImageProcessorManager.process(image, ProcessingMode.COLOR_INVERSION);
+		
+		assertArrayEquals(bytesCopy, bytes, ASSERTION_ERROR_MESSAGE);
+	}
+
+	@Test
+	void invertColorsTestWithAplhaChannel() {
+		BufferedImage image = new BufferedImage(1000, 1200, BufferedImage.TYPE_4BYTE_ABGR);
+		byte[] bytes = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+		byte[] bytesCopy = bytes.clone();
+		
+		ImageProcessor.invertColors(bytesCopy, 4);
+		ImageProcessorManager.process(image, ProcessingMode.COLOR_INVERSION);
+		
+		assertArrayEquals(bytesCopy, bytes, ASSERTION_ERROR_MESSAGE);
+	}
+	
+	@Test
+	void convertToGrayscaleTest() {
+		BufferedImage image = new BufferedImage(1000, 1200, BufferedImage.TYPE_3BYTE_BGR);
+		byte[] bytes = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+		byte[] bytesCopy = bytes.clone();
+		
+		ImageProcessor.convertToGrayscale(bytesCopy, 3);
+		ImageProcessorManager.process(image, ProcessingMode.GRAYSCALE);
+		
+		assertArrayEquals(bytesCopy, bytes, ASSERTION_ERROR_MESSAGE);
+	}
+	
+	@Test
+	void convertToGrayscaleTestWithAlphaChannel() {
+		BufferedImage image = new BufferedImage(1000, 1200, BufferedImage.TYPE_4BYTE_ABGR);
+		byte[] bytes = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+		byte[] bytesCopy = bytes.clone();
+		
+		ImageProcessor.convertToGrayscale(bytesCopy, 4);
+		ImageProcessorManager.process(image, ProcessingMode.GRAYSCALE);
+		
+		assertArrayEquals(bytesCopy, bytes, ASSERTION_ERROR_MESSAGE);
+	}
+
+}
