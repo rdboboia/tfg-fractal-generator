12419b1cd00565b13d2171f3ef1427452b64d115
Radu Dumitru Boboia <radulik95@gmail.com>
2019-07-20 18:09:26 +0200

    Updated the console APP starter. Now works for any version.

--- console app starter.bat
+++ console app starter.bat
@@ -1,8 +1,24 @@
 @echo off
 
+setlocal enabledelayedexpansion
+
+set file=null
+set fileCount=0
+for %%a in (target\fractalgenerator-*.jar) do (
+	set file=%%a
+	set /a fileCount+=1
+)
+
+if %fileCount% GTR 1 (
+	echo More than one file found.
+	echo Please leave only 1 JAR executable file in the target directory.
+	pause
+	exit
+)
+
 set /p args=Args: 
 echo Args=%args%
-java -jar target\fractalgenerator-0.1.0-SNAPSHOT.jar %args%
+java -jar %file% %args%
 
 pause
 exit
